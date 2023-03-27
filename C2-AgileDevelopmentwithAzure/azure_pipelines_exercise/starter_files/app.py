cc576932c58a762e2dbb07c3725083f486bfac48
gome016 <gomezl.ivan@gmail.com>
2023-03-27 11:21:33 +0000

    update the code

--- C2-AgileDevelopmentwithAzure/azure_pipelines_exercise/starter_files/app.py
+++ C2-AgileDevelopmentwithAzure/azure_pipelines_exercise/starter_files/app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home CD enabled! 27.03.2023 </h3>"
+    html = "<h3>Sklearn Prediction Home CD enabled! 27.03.2023 13:21 </h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
